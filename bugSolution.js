97ec58a39b31fed4bb113ff9d01a2af5dce1ae8f
BugHunter-X <askzenai@gmail.com>
2024-12-13 22:00:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,6 @@
+```javascript
+<div class="bg-gray-100 p-4">
+  <h1 class="text-3xl font-bold text-gray-800">Uncommon Tailwind CSS Bug</h1>
+  <p class="text-gray-800">This is a paragraph with some text. The `text-gray-800` class is now correctly applied.</p>
+</div>
+```
