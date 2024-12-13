aaba7a5a9360a6ad7b7287a255f79678f50b1f60
BugHunter-X <askzenai@gmail.com>
2024-12-13 22:00:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,6 @@
+```javascript
+<div class="bg-gray-100 p-4">
+  <h1 class="text-3xl font-bold">Uncommon Tailwind CSS Bug</h1>
+  <p>This is a paragraph with some text. It uses the Tailwind CSS class `text-gray-800` to set the text color. However, this color is not applied correctly in this situation, resulting in unexpected styling issues.</p>
+</div>
+```
